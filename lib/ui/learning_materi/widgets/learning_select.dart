e6458f64dbad1270586892abeceed6fefbbcd698
Ptrizy <sianipar.sando@gmail.com>
2024-07-23 21:40:51 +0700

    fix: add download file functionality

--- lib/ui/learning_materi/widgets/learning_select.dart
+++ lib/ui/learning_materi/widgets/learning_select.dart
@@ -1,12 +1,81 @@
+import 'dart:async';
+
+import 'package:fl_downloader/fl_downloader.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:quiz/common/theme/app_font_style.dart';
 import 'package:quiz/common/utils/transition_fade.dart';
 import 'package:quiz/common/widgets/custom_button.dart';
+import 'package:quiz/controller/course_controller.dart';
 import 'package:quiz/ui/learning_materi/widgets/learning_materi_content.dart';
 
-class LearningMateriSelect extends StatelessWidget {
+class LearningMateriSelect extends StatefulWidget {
   const LearningMateriSelect({super.key});
+
+  @override
+  State<LearningMateriSelect> createState() => _LearningMateriSelectState();
+}
+
+class _LearningMateriSelectState extends State<LearningMateriSelect> {
+  final CourseController _courseController = Get.find<CourseController>();
+
+  int progress = 0;
+  int? downloadId;
+  String? status;
+  late StreamSubscription progressStream;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeDownloader();
+    _requestPermission();
+  }
+
+  void _initializeDownloader() async {
+    try {
+      await FlDownloader.initialize();
+      progressStream = FlDownloader.progressStream.listen(
+        (event) {
+          print(
+              "Download event: ${event.status}, Progress: ${event.progress}, ID: ${event.downloadId}");
+          setState(() {
+            progress = event.progress;
+            downloadId = event.downloadId;
+            status = event.status.name;
+          });
+
+          if (event.status == DownloadStatus.successful) {
+            print("Download successful. File path: ${event.filePath}");
+            FlDownloader.openFile(filePath: event.filePath);
+          }
+        },
+        onError: (error) {
+          print("Error in download stream: $error");
+          Get.snackbar('Error', 'Terjadi kesalahan saat mengunduh: $error');
+        },
+      );
+    } catch (e) {
+      print("Error initializing downloader: $e");
+      Get.snackbar('Error', 'Gagal menginisialisasi pengunduh: $e');
+    }
+  }
+
+  @override
+  void dispose() {
+    progressStream.cancel();
+    super.dispose();
+  }
+
+  Future<void> _requestPermission() async {
+    final permission = await FlDownloader.requestPermission();
+    if (permission == StoragePermissionStatus.granted) {
+      _courseController.getCoursesByClassId();
+    } else {
+      Get.snackbar('Perizinan Penyimpanan Ditolak',
+          'Izinkan izin terlebih dahulu untuk melihat materi. Status: $permission');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,7 +140,45 @@ class LearningMateriSelect extends StatelessWidget {
                       CustomButton(
                         widget: Text("Download",
                             style: AppFontStyle.mediumLargeText),
-                        onPressed: () {},
+                        onPressed: () async {
+                          if (_courseController.courses.value != null &&
+                              _courseController.courses.value!.isNotEmpty) {
+                            final permission =
+                                await FlDownloader.requestPermission();
+                            if (permission == StoragePermissionStatus.granted) {
+                              final courseDetail =
+                                  _courseController.courses.value!.first;
+                              final url = courseDetail.course;
+                              if (url.isNotEmpty) {
+                                final fileName =
+                                    '${courseDetail.title.replaceAll(' ', '_')}.pdf';
+                                final id = await FlDownloader.download(
+                                  url,
+                                  fileName: fileName,
+                                );
+                                if (id != null) {
+                                  setState(() {
+                                    downloadId = id;
+                                  });
+                                  Get.snackbar('Unduhan Dimulai',
+                                      'File $fileName sedang diunduh');
+                                } else {
+                                  Get.snackbar(
+                                      'Error', 'Gagal memulai unduhan.');
+                                }
+                              } else {
+                                Get.snackbar(
+                                    'Error', 'URL file tidak tersedia.');
+                              }
+                            } else {
+                              Get.snackbar('Perizinan Ditolak',
+                                  'Izin penyimpanan diperlukan untuk mengunduh file.');
+                            }
+                          } else {
+                            Get.snackbar('Error',
+                                'Tidak ada materi kursus yang tersedia.');
+                          }
+                        },
                         height: 70.h,
                         width: 180.w,
                         backgroundColor: Color(0XFFBDF565),
